ec08b4907343cedc6d6be1579e73758f9ee95d24
AchiCatty <bulsago2001@google.com>
2020-11-25 15:42:49 +0900

    Form changed

--- TheProject/TheProject/Form/Search.cs
+++ TheProject/TheProject/Form/Search.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace TheProject
 {
-    public partial class Form1 : Form
+    public partial class Search : Form
     {
-        public Form1()
+        public Search()
         {
             InitializeComponent();
         }
