32de2100de78156fb38fecb058a237d7813c9489
LJH <snrbs17@gmail.com>
2020-11-25 15:49:36 +0900

    1

---
+++ TheProject/ProjectData/EFLibrary/DbContextCreator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFLibrary
+{
+    public class DbContextCreator
+    {
+        public static Func<DbContext> Context { get; set; }
+    }
+}
