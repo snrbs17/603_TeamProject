8172eccca19e01041c9641f29d794c533249156f
yangs84 <72100461+yangs84@users.noreply.github.com>
2020-11-27 10:14:30 +0900

    Add files via upload
    차트 스키마 검토 부탁드려요.

---
+++ DataSet/DataSet1.cs
@@ -0,0 +1,35 @@
+﻿namespace WFchart
+{
+
+
+    partial class DataSet1
+    {
+        partial class RevenueDataTable
+        {
+        }
+
+        partial class Storage_MGDataTable
+        {
+        }
+
+        partial class TimeDataTable
+        {
+        }
+
+        partial class StatusDataTable
+        {
+        }
+
+        partial class Storage_TypeDataTable
+        {
+        }
+
+        partial class RegionalDataTable
+        {
+        }
+
+        partial class Business_UnitDataTable
+        {
+        }
+    }
+}
